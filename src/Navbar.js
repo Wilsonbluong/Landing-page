affed3cd8fa147822000e86778f53c787275528a
Wilson Luong <wilsonbluong@gmail.com>
2021-02-12 20:05:34 -0800

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
+
+import { useGlobalContext } from "./context";
+
+const Navbar = () => {
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+
+  const displaySubmenu = (e) => {
+    // page is where the nav links are in the navbar
+    // tempBtn is the location of the nav links
+    // center/bottom: location of center and bottom of button
+    // pass in page into the openSubmenu function so it knows which page
+    // pass in {center, bottom}
+    const page = e.target.textContent;
+    const tempBtn = e.target.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+    openSubmenu(page, { center, bottom });
+  };
+
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains("link-btn")) {
+      closeSubmenu();
+    }
+  };
+
+  return (
+    <nav className="nav" onMouseOver={handleSubmenu}>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="stripe logo" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              products
+            </button>
+          </li>
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              developers
+            </button>
+          </li>
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              company
+            </button>
+          </li>
+        </ul>
+        {/* <button className="btn sign-in-btn">Sign in</button> */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
